5309c32d1c50bed965bf58eab6ac74ce9cc89f31
jucasoliveira <jucas.oliveira@gmail.com>
2023-01-09 16:00:52 +0000

    add check re-request

--- bin/utils.js
+++ bin/utils.js
@@ -1,5 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
-const { encrypt, saveApiKey, getApiKey } = require("./encrypt");
+const { encrypt, saveApiKey, getApiKey, deleteApiKey } = require("./encrypt");
 const prompts = require("prompts");
 const chalk = require("chalk");
 const process = require("process");
@@ -25,65 +25,97 @@ const apiKeyPrompt = async () => {
   return apiKey;
 };
 
-const generateResponse = async (
-  apiKey,
-  prompt,
-  options,
-  response,
-  context = ""
-) => {
-  const configuration = new Configuration({
-    apiKey,
-  });
-  const openai = new OpenAIApi(configuration);
-  const spinner = ora("Thinking...").start();
-  const request = await openai
-    .createCompletion({
-      model: options.engine || "text-davinci-002",
-      prompt: `${context}\n${response.value}`,
-      max_tokens: 2048,
-      temperature: parseInt(options.temperature) || 0.5,
-    })
-    .then((res) => {
-      spinner.stop();
-      return res;
-    })
-    .catch((err) => {
-      if (err["response"]["status"] == "429") {
-        console.error(
-          `${chalk.red(
-            "\nChat GPT is having too many requests, wait and send it again."
-          )}`
-        );
-      } else {
-        console.error(`${chalk.red("Something went wrong!!")} ${err}`);
-      }
+let context = "";
 
-      spinner.stop();
-      return "error";
+const addContext = (text) => {
+  context = `${context}\n${text}`;
+};
+
+const getContext = () => {
+  return context;
+};
+
+const generateResponse = async (apiKey, prompt, options, response) => {
+  try {
+    const configuration = new Configuration({
+      apiKey,
     });
 
-  if (request == undefined || !request.data?.choices?.[0].text) {
-    console.error(`${chalk.red("Something went wrong!")}`);
-    return "error";
-  }
+    let sessionContext = getContext();
+    console.log(sessionContext, "****");
+    const openai = new OpenAIApi(configuration);
+    const spinner = ora("Thinking...").start();
+    const request = await openai
+      .createCompletion({
+        model: options.engine || "text-davinci-002",
+        prompt: `${response.value}`,
+        max_tokens: 2048,
+        temperature: parseInt(options.temperature) || 0.5,
+      })
+      .then((res) => {
+        spinner.stop();
+        return res;
+      })
+      .catch((err) => {
+        if (err["response"]["status"] == "429") {
+          console.error(
+            `${chalk.red(
+              "\nChat GPT is having too many requests, wait and send it again."
+            )}`
+          );
+        } else {
+          console.error(`${chalk.red("Something went wrong!!")} ${err}`);
+        }
 
-  // map all choices to text
-  const getText = request.data.choices.map((choice) => choice.text);
+        spinner.stop();
+        return "error";
+      });
 
-  console.log(`${chalk.cyan("GPT-3: ")}`);
-  // console log each character of the text with a delay and then call prompt when it finished
-  let i = 0;
-  const interval = setInterval(() => {
-    if (i < getText[0].length) {
-      process.stdout.write(getText[0][i]);
-      i++;
-    } else {
-      clearInterval(interval);
-      console.log("\n");
-      prompt();
+    if (request == undefined || !request.data?.choices?.[0].text) {
+      throw new Error("Something went wrong!");
     }
-  }, 10);
+
+    // map all choices to text
+    const getText = request.data.choices.map((choice) => choice.text);
+
+    addContext(getText[0]);
+    console.log(`${chalk.cyan("GPT-3: ")}`);
+    // console log each character of the text with a delay and then call prompt when it finished
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i < getText[0].length) {
+        process.stdout.write(getText[0][i]);
+        i++;
+      } else {
+        clearInterval(interval);
+        console.log("\n");
+        prompt();
+      }
+    }, 10);
+  } catch (err) {
+    console.error(`${chalk.red("1 Something went wrong!!")} ${err}`);
+    // create a prompt of type select , with the options to exit or try again
+    const response = await prompts({
+      type: "select",
+      name: "value",
+      message: "Try again?",
+      choices: [
+        { title: "Yes", value: "yes" },
+        { title: "No - exit", value: "no" },
+      ],
+      initial: 0,
+    });
+
+    switch (response.value) {
+      case "no":
+        return process.exit(0);
+      case "yes":
+      default:
+        // call the function again
+        generateResponse(apiKey, prompt, options, response);
+        break;
+    }
+  }
 };
 
 module.exports = {
